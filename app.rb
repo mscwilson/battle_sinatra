4a0f1dfa4fe2d1e29154ed189047a2970ae11010
Miranda Wilson <mirandascwilson@gmail.com>
2021-01-21 19:37:55 +0000

    can attack once and go to attack confirmation screen

--- app.rb
+++ app.rb
@@ -3,6 +3,9 @@ require "sinatra/base"
 class Battle < Sinatra::Base
   enable :sessions
 
+  STARTING_HIT_POINTS = 20
+  ATTACK_DAMAGE = 5
+
   get "/" do
     erb(:index)
   end
@@ -17,6 +20,17 @@ class Battle < Sinatra::Base
     @p1name = session[:p1name]
     @p2name = session[:p2name]
     erb :play
+  end
+
+  post "/attack" do
+    # session[:p2hp] = STARTING_HIT_POINTS - ATTACK_DAMAGE
+    redirect "/attack_confirmation"
+  end
+
+  get "/attack_confirmation" do
+    @p1name = session[:p1name]
+    @p2name = session[:p2name]
+    erb :attack_confirmation
   end
 
   run! if app_file == $0
