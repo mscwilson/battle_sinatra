aea1d6f5fe25e98a32ec3dff1855cb3388634258
Miranda Wilson <mirandascwilson@gmail.com>
2021-01-21 18:20:23 +0000

    used post redirect get cycle from form to play.erb

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -2,7 +2,7 @@ feature "testing infrastructure" do
 
   scenario "displays test text" do
     visit "/"
-    expect(page).to have_text("Welcome to Battle!")
+    expect(page).to have_text("Battle time!")
   end
   
 end
